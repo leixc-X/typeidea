1063e9ad4fddd82852baffce79840dfe1ee990eb
lxc1997YE <leixiaochen97@outlook.com>
2019-11-12 17:35:10 +0800

    优化前端页面

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -2,43 +2,69 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 from .models import Post, Category, Tag
+from .adminforms import PostAdminForm
+from typeidea.custom_site import custom_site
+from typeidea.base_admin import BaseOwnerAdmin
+from django.contrib.admin.models import LogEntry
+
 
 # Register your models here.
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count') # 列表展示的内容项
-    fields = ('name', 'status', 'is_nav')   # 控制页面显示的字段
+# 分类页面添加文章功能
+class PostInline(admin.TabularInline):
+    fields = ('title', 'desc')
+    extra = 1   # 控制额外多几个
+    model = Post
+
+@admin.register(Category, site=custom_site)
+class CategoryAdmin(BaseOwnerAdmin):
+    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')  # 列表展示的内容项
+    fields = ('name', 'status', 'is_nav')  # 控制页面显示的字段
+    inlines = [PostInline, ]
 
     def post_count(self, obj):
         return obj.post_set.count()
+
     post_count.short_description = '文章数量'
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(CategoryAdmin, self).save_model(request, obj, form, change)
 
 
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+@admin.register(Tag, site=custom_site)
+class TagAdmin(BaseOwnerAdmin):
     list_display = ('name', 'status', 'created_time')
     fields = ('name', 'status')
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """ 自定义过滤器只展示当前用户分类 """
+
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
+@admin.register(Post, site=custom_site)
+class PostAdmin(BaseOwnerAdmin):
+    form = PostAdminForm
     list_display = [
         'title', 'category', 'status',
         'created_time', 'owner', 'operator'
     ]
     list_display_links = []
 
-    list_filter = ['category', ]
+    list_filter = [CategoryOwnerFilter, ]
     search_fields = ['title', 'category__name']
+    save_on_top = True
 
     actions_on_top = True
     actions_on_bottom = True
@@ -46,21 +72,57 @@ class PostAdmin(admin.ModelAdmin):
     # 编辑页面
     save_on_top = True
 
-    fields = (
-        ('category', 'title'),
-        'desc',
-        'status',
-        'content',
-        'tag',
+    exclude = ('owner', )
+
+    fieldsets = (
+        ('基础配置', {
+            'description': '基础配置描述',
+            'fields': (
+                ('category', 'title'),
+                'status',
+            ),
+
+        }),
+        ('内容', {
+            'fields': (
+                'desc',
+                'content',
+            ),
+        }),
+        ('额外信息', {
+            'classes': ('wide',),
+            'fields': ('tag', ),
+        })
     )
+    filter_vertical = ('tag', )
 
     def operator(self, obj):
         return format_html(
             '<a href="{}">编辑</a>',
-            reverse('admin:blog_post_change', args=(obj.id,))
+            reverse('cus_admin:blog_post_change', args=(obj.id,))
         )
+
     operator.short_description = '操作'
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(PostAdmin, self).save_model(request, obj, form, change)
+    # def save_model(self, request, obj, form, change):
+    #     obj.owner = request.user
+    #     return super(PostAdmin, self).save_model(request, obj, form, change)
+    #
+    # def get_queryset(self, request):
+    #     # 用来过滤掉其他用户创建的文章，用户只能看到自己创建的
+    #     qs = super(PostAdmin, self).get_queryset(request)
+    #     return qs.filter(owner=request.user)
+    # class Media:
+    #     css = {
+    #         'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css", ),
+    #     }
+    #     js = ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js", )
+
+
+# @admin.register(LogEntry, site=custom_site)
+# class LogEntryAdmin(admin.ModelAdmin):
+#     list_display = ['object_rept', 'object_id', 'action_flag', 'user', 'change_message']
+
+
+
+
--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -1,20 +1,16 @@
-from django.db import models
+from __future__ import unicode_literals
+
 from django.contrib.auth.models import User
+from django.db import models
 
-
-# Create your models here.
 
 class Category(models.Model):
-
-    def __str__(self):
-        return self.name
-
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
-    STATUS_ITEMS = {
+    STATUS_ITEMS = (
         (STATUS_NORMAL, '正常'),
         (STATUS_DELETE, '删除'),
-    }
+    )
 
     name = models.CharField(max_length=50, verbose_name="名称")
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
@@ -23,20 +19,19 @@ class Category(models.Model):
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
-        verbose_name = verbose_name_plural = "分类"
-
-
-class Tag(models.Model):
+        verbose_name = verbose_name_plural = '分类'
 
     def __str__(self):
         return self.name
 
+
+class Tag(models.Model):
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
-    STATUS_ITEMS = {
+    STATUS_ITEMS = (
         (STATUS_NORMAL, '正常'),
         (STATUS_DELETE, '删除'),
-    }
+    )
 
     name = models.CharField(max_length=10, verbose_name="名称")
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
@@ -44,18 +39,21 @@ class Tag(models.Model):
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
-        verbose_name = verbose_name_plural = "标签"
+        verbose_name = verbose_name_plural = '标签'
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
     STATUS_DRAFT = 2
-    STATUS_ITEMS = {
+    STATUS_ITEMS = (
         (STATUS_NORMAL, '正常'),
         (STATUS_DELETE, '删除'),
-        (STATUS_DRAFT, "草稿"),
-    }
+        (STATUS_DRAFT, '草稿'),
+    )
 
     title = models.CharField(max_length=255, verbose_name="标题")
     desc = models.CharField(max_length=1024, blank=True, verbose_name="摘要")
@@ -66,6 +64,43 @@ class Post(models.Model):
     owner = models.ForeignKey(User, verbose_name="作者")
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
+    pv = models.PositiveIntegerField(default=1)
+    uv = models.PositiveIntegerField(default=1)
+
     class Meta:
         verbose_name = verbose_name_plural = "文章"
-        ordering = ['-id']  # 根据id对文章进行排序
+
+    def __str__(self):
+        return self.title
+
+    @staticmethod
+    def get_by_tag(tag_id):
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            tag = None
+            post_list = []
+        else:
+            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)\
+                .select_related('owner', 'category')
+        return post_list, tag
+
+    @staticmethod
+    def get_by_category(category_id):
+        try:
+            category = Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            category = None
+            post_list = []
+        else:
+            post_list = category.post_set.filter(status=Post.STATUS_NORMAL)\
+                .select_related('owner', 'category')
+        return post_list, category
+
+    @classmethod
+    def latest_posts(cls):
+        return cls.objects.filter(status=cls.STATUS_NORMAL)
+
+    @classmethod
+    def hot_posts(cls):
+        return cls.objects.filter(status=cls.STATUS_NORMAL).ordery_by('-pv')
